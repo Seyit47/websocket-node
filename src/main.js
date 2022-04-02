d5e46e9a92a29cc408526e0830df66f160e4bc1b
Seyit <seyitbu1111@gmail.com>
2022-04-02 19:11:13 +0500

    feat: some changes

--- src/main.js
+++ src/main.js
@@ -6,31 +6,59 @@ wss.on("listening", () => {
     console.log(`WebSocket Server listening on port ${8080}`)
 })
 
-const clients = new Map();
+const clients = new Set();
 
 wss.on("connection", (ws) => {
     const id = uuidv4();
     const color = Math.floor(Math.random() * 360);
     const metadata = { id, color };
+    ws.metadata = metadata;
 
-    clients.set(ws, metadata);
+    clients.add(ws);
 
     ws.on("message", (messageAsString) => {
         console.log('received: %s', messageAsString);
         const message = JSON.parse(messageAsString);
-        const metadata = clients.get(ws);
 
-        message.sender = metadata.id;
-        message.color = metadata.color;
+        message.sender = ws.metadata.id;
+        message.color = ws.metadata.color;
 
-        const outbound = JSON.stringify(message);
+        console.log(message)
 
-        [...clients.keys()].forEach((client) => {
-            client.send(outbound);
-        });
+        if (message.type === "message") {
+            onMessage(message);
+        }
+
+        if (message.type === "offer") {
+            const outbound = JSON.stringify(message);
+
+            [...clients.keys()].forEach((client) => {
+                if (message.sender !== client.metadata.id) {
+                    client.send(outbound);
+                }
+            });
+        }
+
+        if (message.type === "answer") {
+            const outbound = JSON.stringify(message);
+
+            [...clients.keys()].forEach((client) => {
+                if (message.sender !== client.metadata.id) {
+                    client.send(outbound);
+                }
+            });
+        }
+
+        function onMessage() {
+            const outbound = JSON.stringify(message);
+
+            [...clients.keys()].forEach((client) => {
+                if (message.sender !== client.metadata.id) {
+                    client.send(outbound);
+                }
+            });
+        }
     })
-
-    console.log(clients)
 
     ws.on("close", () => {
         clients.delete(ws);
